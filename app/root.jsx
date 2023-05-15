cedcb118b58dc1ef38349defed72453a3c819dd0
g0dlessjs <juangalarce.dev@gmail.com>
2023-05-15 15:54:02 -0400

    Se crea el layout html y sus estilos

---
+++ app/root.jsx
@@ -0,0 +1,39 @@
+import { Meta, Links } from "@remix-run/react";
+import styles from "./styles/index.css";
+
+export function meta() {
+  return [
+    { charset: "utf-8" },
+    { title: "Guitar - Remix" },
+    { name: "viewport", content: "width=device-width,initial-scale=1" },
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+
+export default function App() {
+  return (
+    <Document>
+      <h1>Hola Mundo desde Remix</h1>
+    </Document>
+  );
+}
+
+function Document({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        <Meta />
+        <Links/>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
